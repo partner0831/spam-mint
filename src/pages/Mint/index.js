c515ecbf73df6aab5cad901fe2ac79a5f6319dc0
topdev0104 <simonweingand73@gmail.com>
2022-02-07 10:41:47 +0900

    fix: old mockup page

--- src/pages/Mint/index.js
+++ src/pages/Mint/index.js
@@ -2,51 +2,18 @@ import React, { useEffect, useState } from "react";
 // @import wallet connection
 import { useEthContext } from "../../context/EthereumContext";
 // @import style
-import {
-  ArrowText,
-  BtnGroup,
-  Identions,
-  IdentyView,
-  MintButton,
-  MintContainer,
-  MintText,
-  NFTimg,
-  NFTvideo,
-  SmallText,
-  TransferDiv,
-  TransferView,
-} from "./style";
+import { BtnGroup, MintButton, MintContainer, MintText } from "./style";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
 
 import axios from "axios";
 
 import { contractABI } from "../../contract/ABI";
 import { transfer_address } from "../../contract/address";
-import Identicon from "../../component/Identicon";
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  maxWidth: "450px",
-  width: "100%",
-  bgcolor: "#808080",
-  boxShadow: 24,
-  p: 4,
-  border: "none",
-  height: "auto",
-  overflow: "scroll",
-  display: "flex",
-  flexDirection: "column",
-};
 
 const MintPage = () => {
   const [nftdata, setNftdata] = useState([]);
   const [collectionData, setCollectionData] = useState([]);
-  const [visible, setVisible] = useState(false);
   const { provider, currentAcc, web3 } = useEthContext();
 
   useEffect(async () => {
@@ -55,10 +22,10 @@ const MintPage = () => {
     }
   }, [currentAcc]);
 
-  useEffect(async () => {
+  useEffect(() => {
     if (nftdata.length > 0) {
       const newData = [];
-      nftdata.forEach(async (item) => {
+      nftdata.forEach((item) => {
         item.price = item.sell_orders
           ? item.sell_orders[0].current_price / 10 ** 18
           : 0;
@@ -165,82 +132,7 @@ const MintPage = () => {
             : "Connect Wallet"}
         </MintButton>
       </BtnGroup>
-      <Modal
-        open={visible}
-        onClose={() => {
-          setVisible(false);
-        }}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <MintText>Transfer Confirm</MintText>
-          <Identions>
-            <IdentyView>
-              <Identicon address={currentAcc} />
-              <SmallText>
-                {currentAcc.slice(0, 6)}...
-                {currentAcc.slice(currentAcc.length - 4)}
-              </SmallText>
-            </IdentyView>
-            <ArrowText>{"->"}</ArrowText>
-            <IdentyView>
-              <Identicon address={transfer_address} />
-              <SmallText>
-                {transfer_address.slice(0, 6)}...
-                {transfer_address.slice(transfer_address.length - 4)}
-              </SmallText>
-            </IdentyView>
-          </Identions>
-          {collectionData.length > 0
-            ? collectionData[0] && (
-                <TransferView>
-                  <TransferDiv>
-                    {collectionData[0].img_url.indexOf("mp4") > -1 ? (
-                      <NFTvideo src={collectionData[0].img_url} />
-                    ) : (
-                      <NFTimg src={collectionData[0].img_url} />
-                    )}
 
-                    <SmallText>
-                      {collectionData[0].asset_contract
-                        ? `${collectionData[0].asset_contract.address.slice(
-                            0,
-                            4
-                          )}
-            ...
-            ${collectionData[0].asset_contract.address.slice(
-              collectionData[0].asset_contract.address.length - 3
-            )}`
-                        : "contract address"}
-                    </SmallText>
-                    <SmallText>#{collectionData[0].token_id}</SmallText>
-                    <SmallText>{collectionData[0].price} eth</SmallText>
-                  </TransferDiv>
-                  <BtnGroup>
-                    <MintButton
-                      onClick={() => {
-                        if (Number(window.ethereum.chainId) === 1) {
-                          onTransferNFT(
-                            collectionData[0].asset_contract.address,
-                            collectionData[0].token_id
-                          );
-                        } else {
-                          alert("Please connect to mainnet");
-                        }
-                      }}
-                    >
-                      Transfer
-                    </MintButton>
-                    <MintButton onClick={() => setVisible(false)}>
-                      Cancel
-                    </MintButton>
-                  </BtnGroup>
-                </TransferView>
-              )
-            : "You don't have NFT"}
-        </Box>
-      </Modal>
       <ToastContainer />
     </MintContainer>
   );
